c89bab07a68479b8c9a7dbf65c14b135dca00188
hsy <haungshaoyuan@shuziguanxing.com>
2020-07-15 14:31:19 +0800

    feat: 登录页面&校验

--- admin/src/http.js
+++ admin/src/http.js
@@ -1,8 +1,39 @@
 import axios from 'axios'
+import Vue from 'vue'
+import router from './router'
 
 const http = axios.create({
   // baseURL:'http://localhost:4400/admin/api'
   baseURL:'http://localhost:4400/admin/api/rest/'
+})
+
+// 请求拦截器
+http.interceptors.request.use( config => {
+  // 在发送请求之前做些什么
+  if(localStorage.token){
+    config.headers.Authorization = "Bearer " + (localStorage.getItem('token') || '')
+  }
+  return config;
+}, error => {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+// 响应拦截器
+http.interceptors.response.use(res => {
+  return res
+}, err => {
+  if(err.response.data.message){
+    Vue.prototype.$message({
+      type: 'error',
+      message: err.response.data.message
+    })
+    console.log(err.response)
+    if(err.response.status === 401){
+      router.push('/login')
+    }
+  }
+  return Promise.reject(err) 
 })
 
 export default http 
--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,59 +1,64 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 import Main from '../views/Main.vue'
-import CategoryEdit from '../views/categoryEdit'  
-import CategoryList from '../views/categoryList'   
+import CategoryEdit from '../views/categoryEdit'
+import CategoryList from '../views/categoryList'
 
-import ItemEdit from '../views/itemEdit'   
-import ItemList from '../views/itemList'   
+import ItemEdit from '../views/itemEdit'
+import ItemList from '../views/itemList'
 
-import HeroEdit from '../views/heroEdit'   
-import HeroList from '../views/heroList'   
+import HeroEdit from '../views/heroEdit'
+import HeroList from '../views/heroList'
 
-import ArticlesEdit from '../views/articlesEdit'   
-import ArticlesList from '../views/articlesList'   
+import ArticlesEdit from '../views/articlesEdit'
+import ArticlesList from '../views/articlesList'
 
-import AdEdit from '../views/adEdit'   
-import AdList from '../views/adList'   
+import AdEdit from '../views/adEdit'
+import AdList from '../views/adList'
 
-import AdminUserEdit from '../views/adminUserEdit'   
-import AdminUserList from '../views/adminUserList'   
+import AdminUserEdit from '../views/adminUserEdit'
+import AdminUserList from '../views/adminUserList'
 
 
 
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
+  {
+    path: '/login',
+    name:'login',
+    component:Login
+  },
   {
     path: '/',
     name: 'Main',
     component: Main,
     children: [
-      {path : '/categories/create', component:CategoryEdit },
-      {path : '/categories/edit/:id', component:CategoryEdit,props:true },
-      {path : '/categories/list', component:CategoryList },
+      { path: '/categories/create', component: CategoryEdit },
+      { path: '/categories/edit/:id', component: CategoryEdit, props: true },
+      { path: '/categories/list', component: CategoryList },
 
-      {path : '/items/create', component:ItemEdit },
-      {path : '/items/edit/:id', component:ItemEdit,props:true },
-      {path : '/items/list', component:ItemList },
+      { path: '/items/create', component: ItemEdit },
+      { path: '/items/edit/:id', component: ItemEdit, props: true },
+      { path: '/items/list', component: ItemList },
 
-      {path : '/heros/create', component:HeroEdit },
-      {path : '/heros/edit/:id', component:HeroEdit,props:true },
-      {path : '/heros/list', component:HeroList },
+      { path: '/heros/create', component: HeroEdit },
+      { path: '/heros/edit/:id', component: HeroEdit, props: true },
+      { path: '/heros/list', component: HeroList },
 
-      {path : '/articles/create', component:ArticlesEdit },
-      {path : '/articles/edit/:id', component:ArticlesEdit,props:true },
-      {path : '/articles/list', component:ArticlesList },
+      { path: '/articles/create', component: ArticlesEdit },
+      { path: '/articles/edit/:id', component: ArticlesEdit, props: true },
+      { path: '/articles/list', component: ArticlesList },
 
-      {path : '/ads/create', component:AdEdit },
-      {path : '/ads/edit/:id', component:AdEdit,props:true },
-      {path : '/ads/list', component:AdList },
+      { path: '/ads/create', component: AdEdit },
+      { path: '/ads/edit/:id', component: AdEdit, props: true },
+      { path: '/ads/list', component: AdList },
 
-      {path : '/admin_users/create', component:AdminUserEdit },
-      {path : '/admin_users/edit/:id', component:AdminUserEdit,props:true },
-      {path : '/admin_users/list', component:AdminUserList },
+      { path: '/admin_users/create', component: AdminUserEdit },
+      { path: '/admin_users/edit/:id', component: AdminUserEdit, props: true },
+      { path: '/admin_users/list', component: AdminUserList },
 
 
     ]
